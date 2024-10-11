9abcb310fd5c21d7012071a2903351c574a66ee0
Carlos Silva <carlos.e.a.silva.002@gmail.com>
2024-10-11 18:09:16 -0300

    Update sequelize.ts

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize: Sequelize = new Sequelize('mysql://avnadmin:AVNS_ER9iJCrMooyfz2CJO-Q@mysql-39875044-carlos-83d8.i.aivencloud.com:20230/defaultdb?ssl-mode=REQUIRED', {
+dotenv.config();
+
+const sequelize: Sequelize = new Sequelize(`mysql://avnadmin:${process.env.DB_PASSWORD}@mysql-39875044-carlos-83d8.i.aivencloud.com:20230/defaultdb?ssl-mode=REQUIRED`, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      require: true, 
-      rejectUnauthorized: false 
+      require: true,
+      rejectUnauthorized: false
     }
   }
 });
